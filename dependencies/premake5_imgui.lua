4e35b32d8190af869f3f0fa9595a53c81e8763eb
Dylan Wijnen <dylan.wijnen1@gmail.com>
2019-09-29 14:07:28 -0700

    Added ImGui to dependencies

---
+++ dependencies/premake5_imgui.lua
@@ -0,0 +1,13 @@
+include "../tools/dragoncore-utils.lua"
+
+local function imgui_project(basedir)
+end
+
+local function imgui_includes(basedir)
+    includedirs { basedir .. "imgui/" }
+end
+
+local function imgui_link()
+end
+
+add_dependency_project("imgui", imgui_project, imgui_includes, imgui_link)
