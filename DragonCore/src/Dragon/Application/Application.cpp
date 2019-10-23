94c00a63661b46311d7a1e9e13915ae965a2f8c1
Dylan Wijnen <dylan.wijnen1@gmail.com>
2019-10-22 21:23:44 -0700

    Fixed a bug where release modes would not build.

--- DragonCore/src/Dragon/Application/Application.cpp
+++ DragonCore/src/Dragon/Application/Application.cpp
@@ -8,13 +8,8 @@
 #include <Dragon/Application/Window/Window.h>
 #include <Dragon/Graphics/Graphics.h>
 
-#if defined(DRAGON_DEBUG)
-	#define APP_DIE(SERVICE, PSERVICE, ...) if(!PSERVICE->Init(__VA_ARGS__)) { DERR("["##SERVICE##"] could not be initialized, Exiting..."); return false; }
-	#define APP_CONTINUE(SERVICE, PSERVICE, ...) if(!PSERVICE->Init(__VA_ARGS__)) { DWARN("["##SERVICE##"] could not be intialized. Acting like nothing happened..."); }
-#else
-	#define APP_DIE(...) 
-	#define APP_CONTINUE(...) 
-#endif
+#define APP_DIE(SERVICE, PSERVICE, ...) if(!PSERVICE->Init(__VA_ARGS__)) { DERR("["##SERVICE##"] could not be initialized, Exiting..."); return false; }
+#define APP_CONTINUE(SERVICE, PSERVICE, ...) if(!PSERVICE->Init(__VA_ARGS__)) { DWARN("["##SERVICE##"] could not be intialized. Acting like nothing happened..."); }
 
 #include <iostream>
 
